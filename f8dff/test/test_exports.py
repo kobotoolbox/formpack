4220e8a2256950e0df12680790455df4bbccdc13
Alex Dorey <dorey415@gmail.com>
2016-03-10 08:51:17 -0500

    test initial export of language headers

--- f8dff/test/test_exports.py
+++ f8dff/test/test_exports.py
@@ -4,13 +4,14 @@ from __future__ import (unicode_literals, print_function,
                         absolute_import, division)
 
 import unittest
+import json
 
 from f8dff.models.formpack.pack import FormPack
 from f8dff.fixtures import build_fixture
 
-import json
 
 customer_satisfaction = build_fixture('customer_satisfaction')
+restaurant_profile = build_fixture('restaurant_profile')
 
 
 class TestFormPackExport(unittest.TestCase):
@@ -21,3 +22,27 @@ class TestFormPackExport(unittest.TestCase):
         values_in_lists = FormPack(**customer_satisfaction)._export_to_lists(options)
         print(json.dumps(values_in_lists, indent=4))
 
+    def test_generator_export_language_headers(self):
+        '''
+        formpack.versions[x].languages is not yet defined
+        '''
+        fp = FormPack(**restaurant_profile)
+        self.assertEqual(len(fp.versions), 2)
+        self.assertEqual(len(fp.versions[1].languages), 2)
+
+        # by default, exports use the question 'name' attribute
+        headers = fp._export_to_lists({})[0][1][0]
+        self.assertEquals(headers, ['restaurant_name', 'location'])
+
+        # the first language in the list is the language that appears first
+        # in the column list. in this case, 'label::english'
+        languages = fp.versions[1].languages
+        headers = fp._export_to_lists({
+            'header_language': languages[0],
+        })[0][1][0]
+        self.assertEquals(headers, ['Restaurant name', 'Location'])
+
+        headers = FormPack(**restaurant_profile)._export_to_lists({
+            'header_language': languages[1],
+            })[0][1][0]
+        self.assertEquals(headers, ['nom du restaurant', 'Location'])
--- f8dff/test/test_formpack_internals.py
+++ f8dff/test/test_formpack_internals.py
@@ -1,5 +1,8 @@
 import unittest
 from f8dff.models.formpack.pack import FormPack
+from f8dff.fixtures import build_fixture
+
+restaurant_profile = build_fixture('restaurant_profile')
 
 
 class TestInternalMethods(unittest.TestCase):
@@ -10,3 +13,13 @@ class TestInternalMethods(unittest.TestCase):
     def test_lookup_none_val(self):
         fp = FormPack(title=None)
         self.assertEqual(fp.lookup('title', 'notnone'), 'notnone')
+
+
+class TestSurveyParsers(unittest.TestCase):
+    def test_fixture_has_languages(self):
+        '''
+        restauraunt_profile@v2 has two languages
+        '''
+
+        fp = FormPack(**restaurant_profile)
+        self.assertEqual(len(fp.versions[1].languages), 2)
