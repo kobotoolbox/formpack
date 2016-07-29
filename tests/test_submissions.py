57b47e2556af233cafec909eff1a3c06703aebdf
Alex Dorey <dorey415@gmail.com>
2016-07-29 15:42:40 -0400

    fixes error in test

--- tests/test_submissions.py
+++ tests/test_submissions.py
@@ -12,16 +12,12 @@ restaurant_profile = build_fixture('restaurant_profile')
 
 class TestSubmissionsToVersions(unittest.TestCase):
     def test_submission_counts_match(self):
-        '''
-        restauraunt_profile@v2 has two translations
-        '''
-
         title, schemas, submissions = restaurant_profile
         fp = FormPack(schemas, title)
 
         self.assertEqual(len(fp[1].translations), 2)
 
-        report = fp.autoreport_all_versions()
+        report = fp.autoreport(versions=fp.versions.keys())
         stats = report.get_stats(submissions)
         assert stats.submissions_count == len(submissions)
         assert stats.submission_counts_by_version == {
