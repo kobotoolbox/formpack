3980c7d4045492d11ab91a4e3f4b5aaf898209b3
John N. Milner <john@tmoj.net>
2018-03-01 18:53:52 -0500

    Clean up a few minor things

--- tests/fixtures/field_position_with_multiple_versions/__init__.py
+++ tests/fixtures/field_position_with_multiple_versions/__init__.py
@@ -3,6 +3,14 @@
 from __future__ import (unicode_literals, print_function,
                         absolute_import, division)
 
+'''
+field_position_with_multiple_versions:
+
+ * v1: `Fullname` and `Age` in repeating group
+ * v2: Add `City` after repeating group
+ * v3: Replace `Fullname` with `Firstname`, `Lastname`, and `Gender`
+
+'''
 
 from ..load_fixture_json import load_fixture_json
 
