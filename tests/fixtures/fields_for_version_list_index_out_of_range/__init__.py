d6a813d54c33b734cd26c57de21c41c2d990f170
Olivier Léger <olivierleger@gmail.com>
2018-03-09 11:14:12 -0500

    Added a unittest for 'list index out of range' bug in 'get_fields_for_versions' method

---
+++ tests/fixtures/fields_for_version_list_index_out_of_range/__init__.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+
+from __future__ import (unicode_literals, print_function,
+                        absolute_import, division)
+
+'''
+fields_for_version_list_index_out_of_range:
+
+ * v1: Three text fields: `first_but_not_one`, `one`, `third`
+ * v2: Remove `first_but_not_one` and `third
+ 
+'''
+
+from ..load_fixture_json import load_fixture_json
+
+DATA = {
+    u'title': 'Fields for version list index out of range',
+    u'id_string': 'fields_for_version_list_index_out_of_range',
+    u'versions': [
+        load_fixture_json('fields_for_version_list_index_out_of_range/v1'),
+        load_fixture_json('fields_for_version_list_index_out_of_range/v2'),
+    ],
+}
