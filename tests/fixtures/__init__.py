075ed93d455259338fc5630e315b36ea9880fcd8
Olivier Léger <olivierleger@gmail.com>
2019-08-16 10:21:39 -0400

    Added '__future__'  imports for Python3 compatibility in tests fixtures

--- tests/fixtures/__init__.py
+++ tests/fixtures/__init__.py
@@ -1,2 +1,6 @@
-from load_fixture_json import load_fixture_json  # noqa
-from build_fixture import build_fixture, open_fixture_file  # noqa
+# coding: utf-8
+from __future__ import (unicode_literals, print_function,
+                        absolute_import, division)
+
+from .load_fixture_json import load_fixture_json  # noqa
+from .build_fixture import build_fixture, open_fixture_file  # noqa
--- tests/fixtures/load_fixture_json.py
+++ tests/fixtures/load_fixture_json.py
@@ -1,3 +1,7 @@
+# coding: utf-8
+from __future__ import (unicode_literals, print_function,
+                        absolute_import, division)
+
 import os
 import json
 CUR_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -5,4 +9,5 @@ CUR_DIR = os.path.dirname(os.path.abspath(__file__))
 
 def load_fixture_json(fname):
     with open(os.path.join(CUR_DIR, '%s.json' % fname)) as ff:
-        return json.loads(ff.read().decode('utf8'))
+        content_ = ff.read()
+        return json.loads(content_)
