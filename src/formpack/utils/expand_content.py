2eedaa7865d082284674012a9d3a5223c11bf45e
Michał Łazowik <m1@qed.ai>
2020-05-02 02:12:34 +0200

    Improve perforamance of _pluck_uniq_cols
    Use a set instead of an OrderedDict with empty values for gathering
    unique values.
    
    O(n^2) -> O(nlogn)

--- src/formpack/utils/expand_content.py
+++ src/formpack/utils/expand_content.py
@@ -158,7 +158,7 @@ def _get_special_survey_cols(content):
         'hint::English',
     For more examples, see tests.
     """
-    uniq_cols = OrderedDict()
+    uniq_cols = set()
     special = OrderedDict()
 
     known_translated_cols = content.get('translated', [])
@@ -169,7 +169,7 @@ def _get_special_survey_cols(content):
             # to be parsed and translated in a previous iteration
             _cols = [r for r in row.keys() if r not in known_translated_cols]
 
-            uniq_cols.update(OrderedDict.fromkeys(_cols))
+            uniq_cols.update(_cols)
 
     def _mark_special(**kwargs):
         column_name = kwargs.pop('column_name')
@@ -178,7 +178,7 @@ def _get_special_survey_cols(content):
     _pluck_uniq_cols('survey')
     _pluck_uniq_cols('choices')
 
-    for column_name in uniq_cols.keys():
+    for column_name in uniq_cols:
         if column_name in ['label', 'hint']:
             _mark_special(column_name=column_name,
                           column=column_name,
