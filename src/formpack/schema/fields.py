b4e5abe0d6218304bd63836ca41d2e009b9407d9
Alex Dorey <dorey415@gmail.com>
2021-06-17 13:53:47 -0400

    added a little specificity and clarity

--- src/formpack/schema/fields.py
+++ src/formpack/schema/fields.py
@@ -160,7 +160,10 @@ class FormField(FormDataDef):
             # pyxform#472 introduced dynamic list_names for select_one with the
             # format of `select_one ${question_name}`. The choices are
             # therefore not within a separate choice list
-            if not choice_id.startswith('$'):
+            if choice_id.startswith('${') and choice_id.endswith('}'):
+                # ${dynamic_choice}, so question will be treated as a TextField
+                choice = None
+            else:
                 choice = field_choices[choice_id]
 
         data_type_classes = {
