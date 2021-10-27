3ced41a32243a44fabf5e56ff80c5382c8d7ffc9
Joshua Beretta <joshuaberetta@gmail.com>
2021-10-27 17:36:18 +0000

    change how fields are combined across versions for repeat groups

--- src/formpack/pack.py
+++ src/formpack/pack.py
@@ -247,11 +247,11 @@ class FormPack:
         #
         # Index 0 of tmp2d will be `[field1, field2]`
         tmp2d = []
-        # This dict is used to remember final position of each field.
-        # Its keys are field_names and values are tuples of coordinates in tmp2d
-        # Keeping example above:
-        #       `positions[field1.name]` would be `(0, 0)`
-        #       `positions[field2.name]` would be `(0, 1)`
+        # This dict is used to remember final position of each field.  Its keys
+        # are the combination of field and section names and the values are
+        # tuples of coordinates in tmp2d Keeping example above:
+        #       `positions[f'{field1.name}_{section.name}']` would be `(0, 0)`
+        #       `positions[f'{field2.name}_{section.name}']` would be `(0, 1)`
         positions = {}
 
         # Create the initial field mappings from the first form version
@@ -262,10 +262,11 @@ class FormPack:
         index = 0
         for section in versions_desc[0].sections.values():
             for field_name, field_object in section.fields.items():
+                field_section_name = f'{field_name}_{section.name}'
                 if isinstance(field_object, CopyField):
                     copy_fields.append(field_object)
                 else:
-                    positions[field_name] = (index, 0)
+                    positions[field_section_name] = (index, 0)
                     tmp2d.append([field_object])
                     index += 1
 
@@ -273,9 +274,10 @@ class FormPack:
             index = 0
             for section_name, section in version.sections.items():
                 for field_name, field_object in section.fields.items():
+                    field_section_name = f'{field_name}_{section_name}'
                     if not isinstance(field_object, CopyField):
-                        if field_name in positions:
-                            position = positions[field_name]
+                        if field_section_name in positions:
+                            position = positions[field_section_name]
                             latest_field_object = tmp2d[position[0]][
                                 position[1]
                             ]
