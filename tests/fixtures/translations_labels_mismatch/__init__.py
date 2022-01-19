c28910921e6cc518d850349e8b7c99d980027aae
Joshua Beretta <joshuaberetta@gmail.com>
2022-01-19 21:46:26 +0000

    add failing test

---
+++ tests/fixtures/translations_labels_mismatch/__init__.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+'''
+translations_labels_mismatch
+'''
+
+from ..load_fixture_json import load_fixture_json
+
+DATA = {
+    'title': 'Your favourite Roman emperors',
+    'id_string': 'translations_labels_mismatch',
+    'versions': [
+        load_fixture_json('translations_labels_mismatch/v1'),
+    ],
+}
