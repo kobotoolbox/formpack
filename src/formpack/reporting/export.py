4da4ec94b0b0c1b8b5dc7d9d9b95929e1a8abd7f
Joshua Beretta <joshuaberetta@gmail.com>
2022-06-01 10:25:46 +0000

    remove duplicate inserting of analysis fields

--- src/formpack/reporting/export.py
+++ src/formpack/reporting/export.py
@@ -387,9 +387,6 @@ class Export:
         row = self._row_cache[_section_name]
         _fields = tuple(current_section.fields.values())
 
-        if self.analysis_form:
-            _fields = self.analysis_form.insert_analysis_fields(_fields)
-
         def _get_attachment(val, field, attachments):
             """
             Filter attachments for filenames that match the submission field's
