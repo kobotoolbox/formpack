e639012a02a4911ebc23b2059fa96f99cfc514ae
rgraber <becca.graber@kobotoolbox.org>
2026-02-18 13:15:28 -0500

    fixup!: add file

---
+++ src/formpack/utils/dft.py
@@ -0,0 +1,20 @@
+
+from formpack.schema import FormField
+
+# Basic recursive depth-first traversal
+def dft_recurse(root:FormField, tree: dict[str, list[FormField]], process_field):
+    seen = set()
+    result = [root]
+    seen.add(root.path)
+    for child in tree[root.path]:
+        dft_recurse_inner(child, tree, process_field, result, seen)
+    return result
+
+def dft_recurse_inner(root:FormField, tree: dict[str, list[FormField]], process_field, result, seen):
+    if root.path in seen:
+        return
+    result.append(process_field(root))
+    seen.add(root.path)
+    for child in tree[root.path]:
+        dft_recurse_inner(child, tree, process_field, result, seen)
+
